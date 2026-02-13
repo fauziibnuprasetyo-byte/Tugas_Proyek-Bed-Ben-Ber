d3c69614998efbab21049dedf23091690098002b
Fauzi Ibnu Prasetyo <fauziibnuprasetyo@smkitihsanulfikri.sch.id>
2026-02-13 10:25:06 +0700

    nambah fitur lihat kunjungan

--- sistem-pelayanan-uks/src/main.py
+++ sistem-pelayanan-uks/src/main.py
@@ -1,1 +1,68 @@
-print("SISTEM UKS")
+import sqlite3
+import datetime
+import os
+import csv
+
+DB = "uks.db"
+
+# =======================
+# DAFTAR OBAT
+# =======================
+DAFTAR_OBAT = [
+    "Paracetamol",
+    "Bodrex",
+    "Antimo",
+    "Oralit",
+    "Tolak Angin",
+    "Minyak Kayu Putih",
+    "Obat Maag",
+    "Tidak pakai obat"
+]
+
+# =======================
+# SETUP DATABASE
+# =======================
+conn = sqlite3.connect(DB)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS kunjungan (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+tanggal TEXT,
+nama TEXT,
+kelas TEXT,
+keluhan TEXT,
+tindakan TEXT
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS stok (
+obat TEXT PRIMARY KEY,
+jumlah INTEGER
+)
+""")
+
+for obat in DAFTAR_OBAT:
+    cur.execute("INSERT OR IGNORE INTO stok VALUES (?, ?)", (obat, 20))
+
+conn.commit()
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def lihat_kunjungan():
+    clear()
+    print("=== DATA KUNJUNGAN ===\n")
+
+    cur.execute("SELECT * FROM kunjungan ORDER BY id DESC")
+    data = cur.fetchall()
+
+    if not data:
+        print("Belum ada data.")
+    else:
+        for d in data:
+            print(d)
+
+    input("\nTekan Enter...")
+
